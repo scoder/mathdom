506340c29d94d1c1e7b9d0863e591636bc7d32a6
Stefan Behnel <scoder@users.berlios.de>
2005-09-28 20:03:40 +0000

    moved source to src dir

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 setup(
     name='mathml',
     version='0.1',
-    package_dir={'mathml': ''},
-    packages=['mathml'],
-    py_modules=['mathml.mathdom', 'mathml.termparser', 'mathml.xmlterm']
+    description='Python Distribution Utilities',
+    author='Stefan Behnel',
+    author_email='scoder@users.sourceforge.net',
+    url='http://www.sourceforge.net/projects/mathdom',
+    package_dir={'mathml': 'src'},
+    packages=['mathml']
     )
