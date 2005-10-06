d28d24d50eb9b1777933b9146bc5f3de48c61ef6
Stefan Behnel <scoder@users.berlios.de>
2005-10-06 05:02:47 +0000

    use mark-up in description

--- setup.py
+++ setup.py
@@ -8,27 +8,33 @@ setup(
     description='MathDOM - Content MathML in Python',
     long_description="""MathDOM - Content MathML in Python
 
-MathDOM is a set of Python 2.4 modules (using PyXML and pyparsing)
-that import mathematical terms as a Content MathML DOM. It
-currently parses MathML and literal infix terms into a DOM
-document and writes out MathML and literal infix/prefix/postfix
-terms. The DOM elements are enhanced by domain specific methods
-that make using the DOM a little easier.
+**MathDOM** is a set of Python 2.4 modules (using PyXML_ and
+pyparsing_) that import mathematical terms as a `Content MathML`_ DOM. It
+currently parses MathML and literal infix terms into a DOM document
+and writes out MathML and literal infix/prefix/postfix/Python
+terms. The DOM elements are enhanced by domain specific methods that
+make using the DOM a little easier.
+
+.. _pyparsing:             http://pyparsing.sourceforge.net
+.. _PyXML:                 http://pyxml.sourceforge.net
+.. _`Content MathML`:      http://www.w3.org/TR/MathML2/chapter4.html
+.. _MathML:                http://www.w3.org/TR/MathML2/
 
 You can call it the shortest way between different term
-representations and a Content MathML DOM. Ever noticed the
-annoying differences between terms in different programming
-languages? Build your application around the DOM and stop caring
-about the term representation that users prefer or that your
-machine can execute. If you need a different representation, add a
-converter, but don't change the model. Literal terms are connected
-through an intermediate AST step that makes writing converters for
-Python/SQL/yourfavorite easier.
+representations and a Content MathML DOM. Ever noticed the annoying
+differences between terms in different programming languages? Build
+your application around the DOM and stop caring about the term
+representation that users prefer or that your machine can execute. If
+you need a different representation, add a converter, but don't change
+the model. Literal terms are connected through an intermediate AST
+step that makes writing converters for SQL/Java/Lisp/*your-favourite*
+easy.
 """,
 
     author='Stefan Behnel',
     author_email='scoder@users.sourceforge.net',
     url='http://mathdom.sourceforge.net/',
+    download_url='http://www.sourceforge.net/projects/mathdom',
 
     classifiers = [
     'Development Status :: 3 - Alpha',
@@ -38,5 +44,7 @@ Python/SQL/yourfavorite easier.
     'Programming Language :: Python',
     'Topic :: Text Processing :: Markup :: XML',
     'Topic :: Scientific/Engineering :: Mathematics',
-    ]
+    ],
+
+    keywords = "MathML xml DOM math parser"
     )
