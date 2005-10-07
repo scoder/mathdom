396c5f28836918fbc621a1f3ee1984d883c14cef
Stefan Behnel <scoder@users.berlios.de>
2005-10-07 09:05:39 +0000

    mathml.utils /is/ a package, I guess ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='mathdom',
     version='0.5.0',
-    packages=['mathml'],
+    packages=['mathml', 'mathml.utils'],
 
     description='MathDOM - Content MathML in Python',
     long_description="""MathDOM - Content MathML in Python
