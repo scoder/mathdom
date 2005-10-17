15f6af2b57ea9e29c567ec4c5b4a9a6235fc98da
Stefan Behnel <scoder@users.berlios.de>
2005-10-17 13:56:39 +0000

    documentation updates

--- html/copy.sh
+++ html/copy.sh
@@ -7,4 +7,4 @@ echo "Current version: $VERSION"
 sed -e "s|\(<!--CURRENT-->\)[^<]*\(<!--/CURRENT-->\)|\1$VERSION\2|" > index.html < MathDOM.html
 ls -l index.html
 
-rsync -ruvtl index.html *.png scoder@shell.sourceforge.net:/home/groups/m/ma/mathdom/htdocs/
+rsync -ruvtl index.html *.png *.css scoder@shell.sourceforge.net:/home/groups/m/ma/mathdom/htdocs/
