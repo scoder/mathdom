ac722a57fa5559b82325180b8f6ee726a6f8e342
Stefan Behnel <scoder@users.berlios.de>
2005-10-17 11:34:54 +0000

    adapt description to 0.5.2

--- setup.py
+++ setup.py
@@ -7,13 +7,15 @@ setup(
     description='MathDOM - Content MathML in Python',
     long_description="""MathDOM - Content MathML in Python
 
-**MathDOM** is a set of Python 2.4 modules (using PyXML_ and
-pyparsing_) that import mathematical terms as a `Content MathML`_ DOM. It
-currently parses MathML and literal infix terms into a DOM document
-and writes out MathML and literal infix/prefix/postfix/Python
+**MathDOM** is a set of Python 2.4 modules (using PyXML_ or lxml_, and
+pyparsing_) that import mathematical terms as a `Content MathML`_
+DOM. It currently parses MathML and literal infix terms into a DOM
+document and writes out MathML and literal infix/prefix/postfix/Python
 terms. The DOM elements are enhanced by domain specific methods that
-make using the DOM a little easier.
+make using the DOM a little easier. Implementations based on PyXML and
+lxml/libxml2 are available.
 
+.. _lxml:                  http://codespeak.net/lxml/
 .. _pyparsing:             http://pyparsing.sourceforge.net
 .. _PyXML:                 http://pyxml.sourceforge.net
 .. _`Content MathML`:      http://www.w3.org/TR/MathML2/chapter4.html
@@ -25,9 +27,9 @@ differences between terms in different programming languages? Build
 your application around the DOM and stop caring about the term
 representation that users prefer or that your machine can execute. If
 you need a different representation, add a converter, but don't change
-the model. Literal terms are connected through an intermediate AST
-step that makes writing converters for SQL/Java/Lisp/*your-favourite*
-easy.
+the model of your application. Literal terms are connected through an
+intermediate AST step that makes writing converters for
+SQL/Java/Lisp/*your-favourite* easy.
 """,
 
     author='Stefan Behnel',
