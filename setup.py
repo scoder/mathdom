7f1582c110832adb6b17e7432d73b2e6fcbebeed
Stefan Behnel <scoder@users.berlios.de>
2005-11-03 08:20:28 +0000

    0.6.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='mathdom',
-    version='0.6.2.1',
+    version='0.6.3',
     packages=['mathml', 'mathml.utils', 'mathml.pmathml'],
     package_data = {
     'mathml'       : ['schema/mathml2.rng.gz'],
