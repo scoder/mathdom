29eb99b31e57ae8afbe764f91f2d0d2a900247ea
Stefan Behnel <scoder@users.berlios.de>
2005-11-13 15:11:19 +0000

    simplistic SQL term serializer

---
+++ mathml/utils/sqlterm.py
@@ -0,0 +1,30 @@
+from mathml.termbuilder import tree_converters, InfixTermBuilder
+
+__all__ = [ 'SqlTermBuilder' ]
+
+# BUILDER
+
+class SqlTermBuilder(InfixTermBuilder):
+    _NAME_MAP = {
+        u'e'     : u'exp(1.0)',
+        u'pi'    : u'pi()',
+        u'true'  : u'TRUE',
+        u'false' : u'FALSE'
+        }
+    map_name = _NAME_MAP.get
+
+    def _handle_name(self, operator, operands, affin):
+        name = unicode(str(operands[0]), 'ascii')
+        return [ self.map_name(name, name) ]
+
+    def _handle_const_bool(self, operator, operands, affin):
+        return [ operands[0] and 'TRUE' or 'FALSE' ]
+
+    def _handle_const_complex(self, operator, operands, affin):
+        raise NotImplementedError, "Complex numbers cannot be converted to SQL."
+
+    def _handle_interval(self, operator, operands, affin):
+        raise NotImplementedError, "Intervals cannot be converted to SQL."
+
+
+tree_converters.register_converter('sql', SqlTermBuilder())
